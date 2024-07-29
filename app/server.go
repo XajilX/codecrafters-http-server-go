4cc9504cc7ceee01c367d3942f42a736543fce1a
ajil <844902240@qq.com>
2024-07-29 19:31:15 +1000

    Parse path

--- app/server.go
+++ app/server.go
@@ -20,7 +20,6 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	req := make([]byte, 1024)
 	conn.Read(req)
 	reqs := strings.Split(string(req), "\r\n")
