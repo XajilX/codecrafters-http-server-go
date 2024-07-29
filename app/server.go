ae39ecbf1e875ca8f5bbff30d40ad8521bd71ab1
ajil <844902240@qq.com>
2024-07-29 21:59:21 +1000

    read req body

--- app/server.go
+++ app/server.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"os"
 	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -49,16 +50,17 @@ func Handler(conn net.Conn) {
 		os.Exit(1)
 	}
 	meth, path, prot := reql[0], reql[1], reql[2]
-	if meth != "GET" {
-		conn.Write([]byte(prot + " 404 Not Found\r\n\r\n"))
-		os.Exit(0)
+	if meth == "GET" {
+		Handler_get(conn, prot, path, reqs)
+	} else if meth == "POST" {
+
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-	ua := ""
-	for _, s := range reqs {
-		if strings.HasPrefix(s, "User-Agent: ") {
-			ua, _ = strings.CutPrefix(s, "User-Agent: ")
-		}
-	}
+	conn.Close()
+}
+
+func Handler_get(conn net.Conn, prot, path string, reqs []string) {
 	path_seg := strings.Split(path, "/")
 	len_seg := len(path_seg)
 	if len_seg == 2 && path_seg[1] == "" {
@@ -67,11 +69,47 @@ func Handler(conn net.Conn) {
 		str := path_seg[2]
 		conn.Write(Resp_text_plain(prot, str))
 	} else if len_seg == 2 && path_seg[1] == "user-agent" {
+		ua := ""
+		for _, s := range reqs {
+			if strings.HasPrefix(s, "User-Agent: ") {
+				ua, _ = strings.CutPrefix(s, "User-Agent: ")
+			}
+		}
 		conn.Write(Resp_text_plain(prot, ua))
 	} else if len_seg == 3 && path_seg[1] == "files" {
 		conn.Write(Resp_file(prot, path_seg[2]))
 	} else {
 		conn.Write([]byte(prot + " 404 Not Found\r\n\r\n"))
+	}
+}
+
+func Handler_post(conn net.Conn, prot, path string, reqs []string) {
+	path_seg := strings.Split(path, "/")
+	len_seg := len(path_seg)
+	if len_seg == 3 && path_seg[1] == "files" {
+		len_body := 0
+		for _, s := range reqs {
+			if strings.HasPrefix(s, "User-Agent: ") {
+				str_len_body, _ := strings.CutPrefix(s, "User-Agent: ")
+				len_body, _ = strconv.Atoi(str_len_body)
+			}
+		}
+
+		dir := os.Args[2]
+		file, err := os.Create(dir + path_seg[2])
+		if err != nil {
+			fmt.Println("Error creating file")
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			return
+		}
+		defer file.Close()
+		_, err = file.Write([]byte(reqs[len(reqs)-1])[:len_body])
+		if err != nil {
+			fmt.Println("Error writing file")
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			return
+		}
+		conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
 	}
 }
 
